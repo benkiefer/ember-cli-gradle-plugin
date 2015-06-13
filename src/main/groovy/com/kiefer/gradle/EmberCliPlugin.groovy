80d3788a115fa38fc8c37d764fe0748fd3558063
Ben Kiefer <kieferfam@msn.com>
2015-06-12 21:31:17 -0500

    finish up clean task

--- src/main/groovy/com/kiefer/gradle/EmberCliPlugin.groovy
+++ src/main/groovy/com/kiefer/gradle/EmberCliPlugin.groovy
@@ -2,14 +2,14 @@ package com.kiefer.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
 
 class EmberCliPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
 
-        project.task('clean') << {
-            delete "$project.rootDir/tmp"
-            delete "$project.rootDir/dist"
+        project.tasks.create(name: 'clean', type: Delete) {
+            delete "tmp", "dist"
         }
 
     }
--- src/test/groovy/com/kiefer/gradle/EmberCliPluginTest.groovy
+++ src/test/groovy/com/kiefer/gradle/EmberCliPluginTest.groovy
@@ -2,18 +2,52 @@ package com.kiefer.gradle
 
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
+import static org.testng.Assert.assertFalse
 import static org.testng.Assert.assertNotNull
+import static org.testng.Assert.assertTrue
 
 class EmberCliPluginTest {
+    private Project project;
+    private File projectDir;
+
+    @BeforeMethod
+    void setUp() {
+        projectDir = File.createTempDir()
+        projectDir.mkdir()
+
+        project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+
+        project.pluginManager.apply "com.kiefer.gradle.embercli"
+    }
+
+    @AfterMethod
+    void tearDown() {
+        if (projectDir.exists()) {
+            projectDir.delete()
+        }
+    }
 
     @Test
     void tasksAreRegistered() {
-        Project project = ProjectBuilder.builder().build()
-        project.pluginManager.apply "com.kiefer.gradle.embercli"
-
         assertNotNull(project.tasks.clean)
     }
+
+    @Test
+    void cleanNukesDirectories() {
+        def app = project.mkdir("app")
+        def tmp = project.mkdir("tmp")
+        def dist = project.mkdir("dist")
+
+        project.tasks.clean.execute()
+
+        assertTrue(app.exists())
+        assertFalse(dist.exists())
+        assertFalse(tmp.exists())
+    }
+
 
 }
