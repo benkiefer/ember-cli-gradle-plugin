03d18eb348a0ba2214407958dc3f0c9fd32e141f
Ben Kiefer <kieferfam@msn.com>
2015-06-12 21:00:02 -0500

    keep plugging away at setting up the build infrastructure

---
+++ src/test/groovy/com/kiefer/gradle/EmberCliPluginTest.groovy
@@ -0,0 +1,15 @@
+package com.kiefer.gradle
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+import static org.testng.Assert.assertEquals
+
+class EmberCliPluginTest {
+
+    @Test
+    void doIt() {
+        assertEquals(1, 1);
+    }
+
+}
